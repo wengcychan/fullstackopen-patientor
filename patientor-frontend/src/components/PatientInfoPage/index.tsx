51f26170b0d4977a26bd4a82e653849a17a07ffa
root <root@LAPTOP-01SJFTC3.localdomain>
2024-01-10 16:18:08 +0200

    fix: remove invalid id

--- patientor-frontend/src/components/PatientInfoPage/index.tsx
+++ patientor-frontend/src/components/PatientInfoPage/index.tsx
@@ -21,18 +21,16 @@ const PatientInfoPage = ({diagnoses}: {diagnoses: Diagnosis[]}) => {
 
   useEffect(() => {
     const fetchPatient = async () => {
-      if (!id)
-        return <p>Invalid id</p>;  
-      const patient = await patientService.getPatient(id);
-      if (patient)
-        setPatient(patient);
+      if (id)
+      {
+        const patient = await patientService.getPatient(id);
+        if (patient)
+          setPatient(patient);
+      }
     };
     void fetchPatient();
 
   }, [id]);
-
-  if (!patient)
-    return <p>Invalid id</p>;
 
   const generateGenderIcon = (gender: Gender) => {
     if (gender === Gender.Male)
@@ -43,18 +41,21 @@ const PatientInfoPage = ({diagnoses}: {diagnoses: Diagnosis[]}) => {
       return <TransgenderIcon/>;
   };
 
-  return (
-    <div>
-      <h2>{patient.name} <span>{generateGenderIcon(patient.gender)}</span></h2>
-      <p>SSN: {patient.ssn}</p>
-      <p>Occupation: {patient.occupation}</p>
-      {showAddEntriesForm && <AddEntriesForm patient={patient} setPatient={setPatient} setShowAddEntriesForm={setShowAddEntriesForm} diagnoses={diagnoses}/>}
-      <Entries patient={patient} diagnoses={diagnoses}  />
-      <Button variant="contained" onClick={() => setShowAddEntriesForm(true)}>
-				Add new entry
-			</Button>
-    </div>
-  );
+  if (patient)
+  {
+    return (
+      <div>
+        <h2>{patient.name} <span>{generateGenderIcon(patient.gender)}</span></h2>
+        <p>SSN: {patient.ssn}</p>
+        <p>Occupation: {patient.occupation}</p>
+        {showAddEntriesForm && <AddEntriesForm patient={patient} setPatient={setPatient} setShowAddEntriesForm={setShowAddEntriesForm} diagnoses={diagnoses}/>}
+        <Entries patient={patient} diagnoses={diagnoses}  />
+        <Button variant="contained" onClick={() => setShowAddEntriesForm(true)}>
+          Add new entry
+        </Button>
+      </div>
+    );
+  }
 };
 
 export default PatientInfoPage;
